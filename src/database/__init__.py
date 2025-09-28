e489b7ca0c4128296e20f005af287ec83fbe29f9
Torham <torhamdev@gmail.com>
2025-09-28 20:33:58 +0330

    Add basic database configuration

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,23 +1,7 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from .core import get_db, engine, Base
 
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-# Create SessionLocal class
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Create a Base class for models
-Base = declarative_base()
-
-
-# Dependency to get DB session
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+__all__ = [
+    "get_db",
+    "engine",
+    "Base",
+]
